2a06584e70d33f4aefdf9b9b526b0801a7defcab
Eduardo Perottoni <edu.perottoni@gmail.com>
2020-10-27 21:40:04 -0300

    Iniciando validação de formulário com Javascript
    Inicialização do script em Js para validação do, formulário e mudança de estilos de alguns elementos de acordo com o preenchimento pelo usuário

---
+++ script.js
@@ -0,0 +1,8 @@
+//Traqueando o evento de clique nas labels
+//Quando a div estiver em foco, deixar labels e inputs na cor azul
+
+//Traqueando os inputs
+//Quando os dois inputs estiverem preenchidos, habilitar o botão de entrar nas cores branco e azul
+
+//Traqueando evento de clique no botão de entrar
+//Se os campos estiverem preenchidos, exibir um alert no clique
